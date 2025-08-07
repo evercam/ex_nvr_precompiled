4dc9e65beda22a017668fe3369df96617da1b9c2
Billal GHILAS <bilal@evercam.io>
2025-08-07 09:04:17 +0100

    Add build script

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ExNVR Precompiled
 
-Pre-compiled dependencies for ExNVR.
+Pre-compiled dependencies for [ExNVR](https://github.com/evercam/ex_nvr).
+
+## Dependencies
+
+### FFmpeg
+The pre-compiled ffmpeg is available for Linux amd64/386/arm64/armv7 platforms. The following are the only 
+available features:
+
+* Decoders for `h264` and `hevc`.
+* Encoders for `h264` (libx264) and `mjpeg`.
+* libswscale.
---
+++ build.sh
@@ -0,0 +1,58 @@
+X264_GIT="https://code.videolan.org/videolan/x264.git"
+X264_BRANCH=stable
+
+FFMPEG_GIT="https://github.com/FFmpeg/FFmpeg.git"
+FFMPEG_TAG=n7.1.1
+
+BUILD_DIR=$(pwd)/build
+DOWNLOAD_DIR=$(pwd)/dl
+INSTALL_DIR=$(pwd)
+
+ARCH=$(uname -m)
+
+build_x264() {
+    arch=$1
+
+    echo "Building x264 for $arch..."
+    git clone --depth 1 --branch $X264_BRANCH $X264_GIT $DOWNLOAD_DIR/x264
+
+    cd $DOWNLOAD_DIR/x264
+    ./configure --prefix="$BUILD_DIR/x264" --enable-static --enable-strip --enable-pic --disable-cli --disable-opencl
+
+    make -j$(nproc)
+    make install
+}
+
+build_ffmpeg() {
+    arch=$1
+    echo "Building FFmpeg for $arch..."
+    git clone --depth 1 --branch $FFMPEG_TAG $FFMPEG_GIT $DOWNLOAD_DIR/ffmpeg
+
+    export PKG_CONFIG_PATH=$BUILD_DIR/x264/lib/pkgconfig
+
+    cd $DOWNLOAD_DIR/ffmpeg
+
+    ./configure --prefix="$INSTALL_DIR/ffmpeg" --pkg-config-flags="--static" --extra-libs="-lpthread -lm" --enable-gpl \
+                --disable-static --enable-shared --disable-programs --disable-doc --disable-avdevice --disable-avformat --disable-swresample \
+                --disable-postproc --disable-avfilter --disable-everything --enable-encoder=mjpeg --enable-encoder=libx264 --enable-decoder=h264 \
+                --enable-decoder=hevc --enable-libx264 --disable-sdl2 --disable-alsa
+
+    make -j$(nproc)
+    make install
+}
+
+build_x264 $ARCH
+build_ffmpeg $ARCH
+
+for pc in $INSTALL_DIR/ffmpeg/lib/pkgconfig/*.pc; do
+    sed -i '1s|.*|prefix=${pcfiledir}/../..|' "$pc"
+    sed -i '2s|.*|exec_prefix=${prefix}|' "$pc"
+    sed -i '3s|.*|libdir=${prefix}/lib|' "$pc"
+    sed -i '4s|.*|includedir=${prefix}/include|' "$pc"
+    sed -i '/^Libs\.private:/d' "$pc"
+done
+
+echo "Packaging FFmpeg for $arch..."
+cd $INSTALL_DIR
+VERSION=$(printf "%s" "$FFMPEG_TAG" | cut -c2-)
+tar -cvJf ffmpeg-linux-$arch-$VERSION.tar.xz -C $INSTALL_DIR/ffmpeg .
